ab6a0c74a659728be15e527d00d762dc893e6846
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-01-31 09:22:40 -0600

    Cambiada importacion de archivo de variables de entorno

--- src/js/AxiosInstance.js
+++ src/js/AxiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import {invmentApiKey} from "@/env";
+import {invmentApiKey} from "/src/js/env";
 
-console.log(invmentApiKey);
 export default axios.create({
     baseURL: "http://invment.test:8080/api/v1",
     headers: {

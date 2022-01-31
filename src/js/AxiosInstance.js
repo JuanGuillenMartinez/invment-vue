5e9541499353b22ae1a904b0aa45ed6e1339fda3
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-01-30 21:36:48 -0600

    Cambiada ubicacion del apikey

--- src/js/AxiosInstance.js
+++ src/js/AxiosInstance.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import {invmentApiKey} from "@/env";
 
+console.log(invmentApiKey);
 export default axios.create({
     baseURL: "http://invment.test:8080/api/v1",
     headers: {
-        Authorization: "Bearer " + "1|3mVe2xFmmJ5qEahVjhQJjzwsgu5rOLzcVMbJrg1p",
+        Authorization: "Bearer " + invmentApiKey,
     },
 });
